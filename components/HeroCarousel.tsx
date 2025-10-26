d1b182055a52968b7fb20b4e57fb4bfdd003dbf4
PriceWise Developer <developer@pricewise.com>
2025-10-26 22:43:05 +0530

    fix: Improve readability in HeroCarousel component

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,35 +1,34 @@
 "use client"
 
 import React from 'react'
-import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
 
-const heroImages = [{
-    imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'
-},
-{
-    imgUrl: '/assets/images/hero-2.svg', alt: 'bag'
-}, {
-    imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'
-}, {
-    imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'
-}, {
-    imgUrl: '/assets/images/hero-5.svg', alt: 'chair'
-},]
+/**
+ * Hero carousel image configuration
+ */
+const heroImages = [
+  { imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch' },
+  { imgUrl: '/assets/images/hero-2.svg', alt: 'bag' },
+  { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp' },
+  { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer' },
+  { imgUrl: '/assets/images/hero-5.svg', alt: 'chair' },
+];
 
+/**
+ * Hero carousel component displaying rotating product images
+ */
 const HeroCarousel = () => {
     return (
         <div className='hero-carousel'>
             <Carousel
                 showThumbs={false}
-                // autoFocus={true}
                 autoPlay
                 infiniteLoop
                 interval={2000}
                 showArrows={false}
                 showStatus={false}
-
             >
                 {heroImages.map((image) => (
                     <Image
